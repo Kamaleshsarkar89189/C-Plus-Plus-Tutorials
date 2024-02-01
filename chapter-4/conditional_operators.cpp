6f636d498a1241978955cfb4e94366e31c33cb41
Kamaleshsarkar89189 <skamalesh865@gmail.com>
2024-02-01 16:11:28 +0530

    add new file

--- chapter-4/conditional_operators.cpp
+++ chapter-4/conditional_operators.cpp
@@ -7,6 +7,7 @@
     //If statement
     // Doing things conditionally
 #include<iostream>
+#include<string>
 
 int main(){
     bool red = false;
@@ -91,5 +92,93 @@ int main(){
     }else{
         std::cout << "Stop" << std::endl;
     }
+
+    // Else if 
+    // Testing for several different conditions
+    // Tools
+    const int Pen{10};
+    const int Marker{20};
+    const int Eraser{30};
+    const int Ractangle{40};
+    const int Circle{50};
+    const int Ellipse{60};
+
+    int tool{Pen};
+    if(tool == Pen){
+        std::cout << "Active tool is Pen" << std::endl;
+        //Do the actual painting
+    }
+    else if(tool == Marker){
+        std::cout << "Active tool is Marker" << std::endl;
+    }
+    else if(tool == Eraser){
+        std::cout << "Active tool is Eraser" << std::endl;
+    }
+    else if(tool == Ractangle){
+        std::cout << "Active tool is Ractangle" << std::endl;
+    }
+    else if(tool == Circle){
+        std::cout << "Active tool is Circle" << std::endl;
+    }
+    else if(tool == Ellipse){
+        std::cout << "Active tool is Ellipse" << std::endl;
+    }
+
+    std::cout << "Moving on!" << std::endl;
+
+    std::cout << std::endl;
+
+    //Switch
+
+    // int tool{Pen};
+    switch(tool){
+        case Pen: {
+            std::cout << "Active tool is pen" << std::endl;
+        }
+        break;
+
+        case Marker: {
+            std::cout << "Active tool is Marker" << std::endl;
+        }
+        break;
+
+        default: {
+            std::cout << "Can't match any tool!" << std::endl;
+        }
+        break;
+    }
+
+    std::string name {"Kisan"};
+
+    /*
+    Break : The break statement after each case is very important. It stops processing the switch block when a successful case has been
+    found. If the break statement is not there, all the cases following the current case will be executed.
+
+
+    Condition : Integral types and enums: int, long, unsigned short, ect.
+    */
+
+
+    
+
+    int max{};
+    int a{35};
+    int b{45};
+
+    std::cout << std::endl;
+    std::cout << "Using regular if " << std::endl;
+    
+    // if(a > b){
+    //     max = a;
+    // }else{
+    //     max = b;
+    // }
+    
+    // Ternary Operator
+    max = (a > b)? a:b; //Ternaru operator
+    std::cout << "max : " << max << std::endl;
+
+    //Loops
+
     return 0;
 }
